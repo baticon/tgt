2e1c7dd69f7bac430534ddb91b9afdddc48f3de3
baticon <bkkb2016@gmail.com>
2023-08-12 19:08:09 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const DBwithpassword = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+const DBfull = DBwithpassword.replace(
+  '<USERNAMEADMIN>',
+  process.env.USERNAMEADMIN,
+);
+
+mongoose
+  .connect(DBfull, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
+
+const app = require('./app');
+
+// console.log(process.env);
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port} ...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
